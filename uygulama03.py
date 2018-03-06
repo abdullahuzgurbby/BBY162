d84baa9769671a24ee073ae8990219a594e239ee
abdullahuzgurbby <36530315+abdullahuzgurbby@users.noreply.github.com>
2018-03-06 23:10:21 +0300

    Create uygulama03.py

---
+++ uygulama03.py
@@ -0,0 +1,12 @@
+_auther_ = "Abdullah UZGUR"
+
+#Uygulama03
+#Liste ve Sözlük kullanarak telefon defteri oluşturmak.
+
+telefon_defteri = ["Kazım Koyuncu" , "Ebru Gündeş" , "İlhan Mansız" , "Resul Dindar" , "Sabri Sarıoğlu" , "Orçun Madran" ]
+
+print(telefon_defteri)
+
+kişi = {"Kazım Koyuncu" : "0545 222 33 55" , "Ebru Gündeş" : "0505 505 50 05" , "İlhan Mansız" : "0556 566 56 56" , "Resul Dindar" : "0507 123 45 67" , "Sabri Sarıoğlu" : "0507 999 99 99" , "Orçun Madran" : "0555 555 55 55" }
+
+print(kişi [input("Telefon numarasını görmek istediğin ünlü:")])                        
