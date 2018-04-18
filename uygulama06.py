7d4794689c4b1125769f6327af408d6aeb90c181
abdullahuzgurbby <36530315+abdullahuzgurbby@users.noreply.github.com>
2018-04-18 12:27:27 +0300

    Update uygulama06.py

--- uygulama06.py
+++ uygulama06.py
@@ -14,7 +14,7 @@ class tekerleme:
         self.tekerleme.pack()
 
         self.exit = Button(master, text="Kapat",command=master.quit)
-        self.exit.pack()
+        self.exit.pack(side="bottom")
 
     def tekerleme(self):
         tekerlemeler = ("\nGünlerden bir gün Türkçe dersinde\nİşler karıştı tersi tersine\nNokta, virgül, satır başı\nİşin yoksa başını kaşı\nBen yandım, tıp!","\nOcak kıvılcımlandırıcılardan mısın,\nKapı gıcırdatıcılardan mısın?\nNe ocak kıvılcımlandırıcılardanım,\nNe de kapı gıcırdatıcılardanım.","\nÜç tunç tas kayısı hoşafı.\nÜç tuç tas has kayısı hoşafı.\nPaşa tası ile beş has tas kayısı hoşafı.","\nbu tarlaya bir şinik, kekere mekere ekmişler.\nbu tarlaya da bir şinik kekere mekere ekmişler.\nbu tarlaya ekilen bir şinik kekere mekereye boz ala boz başlı pis porsuk dadanmış,\nbu tarlaya ekilen bir şinik kekere mekereye de boz ala boz başlı pis porsuk dadanmış,\no tarlaya ekilen bir şinik kekere mekereye dadanan boz ala boz başlı pis porsuk,... \ndiğer tarlaya ekilen bir şinik kekere mekereye dadanan boz ala boz başlı pis porsuk'a demiş ki,\nsen ne zamandan beri bu tarlaya ekilen bir şinik kekere mekereye dadanan boz ala boz başlı pis porsuksun?\no da ona cevaben!\nsen ne zaman o tarlaya ekilen bir şinik kekere mekereye dadanan boz ala boz başlı pis porsuk san,\nbende o zamandan beri bu tarlaya ekilen bir şinik kekere mekereye dadanan boz ala boz başlı pis porsuğum demiş.","\nÇatalca’da topal çoban çatal yapıp çatal satar.\nNesi için çatalca’da topal çoban çatal yapıp çatal satar ?\nKarı için çatalca’da topal çoban çatal yapıp çatal satar.")
